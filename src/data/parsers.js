ee18c7e281d244d34e1994fadf2531a051cb658d
James <jhyu0210@gmail.com>
2020-12-30 10:29:30 +0900

    Data converting with map and reduce

--- src/data/parsers.js
+++ src/data/parsers.js
@@ -57,16 +57,83 @@ function parseHistoric(historicData){
   },
   ].reduce((prev, next) => {
     const dLength=historicData.filter((d) => d[next.key]).length;
-    console.log("hitoricData",historicData);
     if (dLength > 4) { //why??4?
      console.log("length",dLength,next.key);
       prev.push(parseChart(historicData, next.key, next.label, next.color));
     }
-    console.log("parsehistoric",prev)
-
+    console.log(JSON.stringify(prev,null,2))
     return prev;
   }, []);
 }
+// test for reduce function
+
+// const something=[
+//   {date:1,a:50, b:30, c:21 ,d:72,e:13,f:43},
+//   {date:2,a:51, b:32, c:25 ,d:74,e:12,f:44},
+//   {date:3,a:52, b:33, c:23 ,d:76,e:13,f:45},
+//   {date:4,a:53, b:33, c:22 ,d:71,e:15,f:46},
+//   {date:5,a:53, b:35, c:24 ,d:73,e:16,f:47},
+//   {date:6,a:55, b:36, c:26 ,d:75,e:17,f:48},
+//   {date:7, b:41}
+// ];
+// //wants
+// // const result = 
+// //   {key:"a",[{x:1, y:50},{x:2,y:51},{x:3,y:52},{x:4,y:53},{x:5,y:54},{x:6,y:55}]},
+// //   {key:"b",[{x:1, y:50},{x:2,y:51},{x:3,y:52},{x:4,y:53},{x:5,y:54},{x:6,y:55}]},
+// //   {key:"c",[{x:1, y:50},{x:2,y:51},{x:3,y:52},{x:4,y:53},{x:5,y:54},{x:6,y:55}]},
+// //   {key:"d",[{x:1, y:50},{x:2,y:51},{x:3,y:52},{x:4,y:53},{x:5,y:54},{x:6,y:55}]},
+// // ]
+// // // something.reduce((prev,next)=>{},[]);
+// // const reduceresult=something.reduce((prev,next)=>{
+// //   (prev.push())
+// // },[]);
+
+// function converting(data,key,label,color){
+//   const convertedData = data.map((cd)=>{
+//     return {
+//       x:cd.date,
+//       y:cd[key] || 0,
+//     }
+//   });
+//   // console.log(convertedData);
+//   const res=({
+//     rdata:convertedData,
+//     title:label,
+//     bg_color:color
+//   });
+//   // console.log(res);
+//   return res;
+// }
+
+// //
+//   const arrayInfo = [
+//     {key:"a",label:"A",color:"red"},
+//     {key:"b",label:"B",color:"purple"},
+//     {key:"c",label:"C",color:"yellow"},
+//     {key:"d",label:"D",color:"green"},
+//     {key:"e",label:"E",color:"blue"},
+//     {key:"f",label:"F",color:"dark-blue"},
+//   ];
+// //
+
+// const resultArray = arrayInfo.reduce((prev,next)=>{
+//   prev.push(converting(something,next.key,next.label,next.color));
+//   return prev;
+// },[])
+
+// console.log(JSON.stringify(resultArray,null,2));
+
+// const testData = [];
+// testData.push(converting(something,"a","A","red"));
+// testData.push(converting(something,"b","B","blue"));
+// testData.push(converting(something,"c","C","yello"));
+// testData.push(converting(something,"d","D", "orange"));
+// console.log(JSON.stringify(testData,null,2));
+
+// converting(something,"a","A","red");
+// converting(something,"b","B","blue");
+// converting(something,"c","C","yello");
+// converting(something,"d","D", "orange");
 
 function parseChart(historicData, key, label, color) {
   const chartData = historicData.map((data) => {
